79e0ae02860d14d7875bdf1a56bdc3833612e570
Thanapat Pirmphol <t.pirmphol@gmail.com>
2018-02-22 22:31:45 +0700

    Test

---
+++ instabot.py
@@ -0,0 +1,14 @@
+from src import InstaBot
+
+bot = InstaBot('behappynerd', 'pip2finn',\
+               like_per_day=1000,\
+               media_max_like=50,\
+               media_min_like=5,\
+               tag_list=['กันดั้ม', 'กันพลา', 'โมเดล', 'ของเล่น', 'ของสะสม', 'gundam', 'bandai', 'gunpla', 'gundamthailand', 'starwars', 'japan', 'gundamfront', 'gundambase', 'hbuc', 'hgbf', 'gunplathailand', 'toy', 'hobbby', 'gundammodelkits', 'gunplamodelkits', 'mobilesuit', 'mobilesuitgundam'],\
+               max_like_for_one_tag=100,\
+               follow_per_day=500,\
+               unfollow_per_day=500,\
+               comments_per_day=10,\
+               comment_list=[['coool', 'great shot', 'superb', 'cool', 'fantastic'], ['❤❤❤❤', '!!!!!', '...']],\
+               log_mod=1)
+bot.auto_mod()
