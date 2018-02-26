24e56bafa29e1ece904372cd8f33f7aa5b81e517
Thanapat Pirmphol <t.pirmphol@gmail.com>
2018-02-26 20:03:18 +0700

    no message

--- instabot.py
+++ instabot.py
@@ -7,8 +7,8 @@ bot = InstaBot('behappynerd', 'pip2finn',\
                tag_list=['กันดั้ม', 'กันพลา', 'โมเดล', 'ของเล่น', 'ของสะสม', 'gundam', 'bandai', 'gunpla', 'gundamthailand', 'starwars', 'japan', 'gundamfront', 'gundambase', 'hbuc', 'hgbf', 'gunplathailand', 'toy', 'hobbby', 'gundammodelkits', 'gunplamodelkits', 'mobilesuit', 'mobilesuitgundam'],\
                max_like_for_one_tag=100,\
                follow_per_day=500,\
-               unfollow_per_day=500,\
-               comments_per_day=10,\
+               unfollow_per_day=100,\
+               comments_per_day=100,\
                comment_list=[['coool', 'great shot', 'superb', 'cool', 'fantastic'], ['❤❤❤❤', '!!!!!', '...']],\
                log_mod=1)
 bot.auto_mod()
